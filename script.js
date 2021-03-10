08bcea41cc66fb404babe3e71f0423b51013b0e8
sampassarelli <passarelli.sam@gmail.com>
2021-03-10 17:18:07 -0600

    completed project

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = []
 
 // Unsplash API 
-const count = 10
+const count = 30
 const apiKey = 'AsRTbuad4JfAF5BWQ_uvv4EumoM8r_OClX3BQT0jLuU'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++ 
+  if (imagesLoaded === totalImages) {
+    ready = true
+    loader.hidden = true
+  }
+}
+
+
+// Helper Function to Set Attributes on DOM elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key])
+  }
+}
+
+
 // Create Elements for Links and Photos, and Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0
+  totalImages = photosArray.length
   // Run function for each object in the photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement('a')
-    item.setAttribute('href', photo.links.html)
-    item.setAttribute('target', '_blank')
+    // item.setAttribute('href', photo.links.html)
+    // item.setAttribute('target', '_blank')
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    })
     // Create Image for Photo
     const img = document.createElement('img')
-    img.setAttribute('src', photo.urls.regular)
-    img.setAttribute('alt', photo.alt_description)
-    img.setAttribute('title', photo.alt_description)
+    // img.setAttribute('src', photo.urls.regular)
+    // img.setAttribute('alt', photo.alt_description)
+    // img.setAttribute('title', photo.alt_description)
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    })
+
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded)
+
     // Put image inside anchor element then put both inside imageContainer element
     item.appendChild(img)
     imageContainer.appendChild(item)
@@ -38,6 +74,14 @@ async function getPhotos() {
     // Catch Error Here 
   }
 }
+
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 && ready) {
+    ready = false
+    getPhotos()
+  }
+})
 
 // On load
 getPhotos()
